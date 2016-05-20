5eef1f7ac0531aadb48f854cba1e5a739dda2b36
BRoman <bur.rom.vik@gmail.com>
2016-05-20 17:05:05 +0300

    top-item-mod

---
+++ start.bat
@@ -0,0 +1,6 @@
+@echo off
+start "D:\programs\Sublime text3" "D:\programs\Sublime text3\sublime_text.exe"
+start "D:\programs\Mozilla Firefox" "D:\programs\Mozilla Firefox\firefox.exe"
+start "D:\GitHub\Jediwerstki\node_modules\.bin" gulp
+start "D:\GitHub\Jediwerstki" "D:\GitHub\Jediwerstki"
+start "D:\GitHub\Jediwerstki"
